b6e5fee0aabb383ee26ebfac3ad459ddd4a8cbb7
Tsunami <doanhongson050999@gmail.com>
2021-08-06 14:53:34 +0700

    em Sơn ngáo

---
+++ Case-study-md4/src/main/java/com/codegym/repository/IRoleRepository.java
@@ -0,0 +1,13 @@
+package com.codegym.repository;
+
+import com.codegym.model.Role;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface IRoleRepository extends CrudRepository<Role,Long> {
+
+    Optional<Role> findByName(String name);
+}
